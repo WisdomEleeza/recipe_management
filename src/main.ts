9b58e7598aabf904915e6389496ad861de7d1b9a
Wisdom Eleeza <whiy07@gmail.com>
2024-01-25 06:44:43 +0000

    Configuring swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Use DocumentBuilder to create a new swagger document configuration
+  const config = new DocumentBuilder()
+    .setTitle('Recipes API') // Set the title of the API
+    .setDescription('Recipes API description') // Set the description of the API
+    .setVersion('0.1') // Set the version of the API
+    .build(); // Build the document
   await app.listen(5555);
 }
 bootstrap();
